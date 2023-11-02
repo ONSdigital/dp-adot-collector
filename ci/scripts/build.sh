07ad5f24ffe3cbc65834d830c21e5576c5c82d9b
mismehmood <misbah.mehmood@ext.ons.gov.uk>
2023-11-02 10:32:56 +0000

    updated dockerfile and build file

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash -eux
-cwd=$(pwd)
-
-ls -la
-
-cp build-file/dp-adot-collector dp-adot-collector/adot-collector
-cp key-file/ftb.tar.gz.asc dp-adot-collector
-
-pushd dp-ci/gpg-keys/ci
-  echo "decrypting CI privkey"
-  echo $PRIVATE_KEY_PASSPHRASE | gpg --batch --passphrase-fd 0 privkey.asc
-  gpg --import pubkey privkey
-popd
-
 
 pushd dp-adot-collector
-    chmod 755 adot-collector
-    gpg -d ftb.tar.gz.asc > ftb.tar.gz
-
-    tar -xvf ftb.tar.gz
+  cp Dockerfile.concourse ../build
 popd
-
-cp -r dp-adot-collector/* build

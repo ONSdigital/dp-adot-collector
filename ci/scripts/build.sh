4d3fd1ef571e194335c343f6d2e1a5b11faf226a
Charles Colbourn <charles.colbourn@ext.ons.gov.uk>
2023-12-20 10:38:50 +0000

    corrected indentation

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,10 +5,10 @@ pushd dp-adot-collector
   if [[ "$APPLICATION" == "dp-adot-collector-lb" ]]; then
       cp Dockerfile-lb.concourse ../build/Dockerfile.concourse
       cp config-lb.yml ../build
-    elif [[ "$APPLICATION" == "dp-adot-collector" ]]; then
+  elif [[ "$APPLICATION" == "dp-adot-collector" ]]; then
       cp Dockerfile.concourse ../build
       cp config-aggregator.yml ../build
-    fi
+  fi
 
 popd
 

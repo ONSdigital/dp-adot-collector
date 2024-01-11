57f298c959ef3a5ee5038994f27d3bb027b0a25a
Charles Colbourn <charles.colbourn@ext.ons.gov.uk>
2024-01-11 11:54:18 +0000

    updated build.sh to include correct (temporary) config file

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,7 +4,7 @@ pushd dp-adot-collector
 
   if [[ "$APPLICATION" == "dp-adot-collector-lb" ]]; then
       cp Dockerfile-lb.concourse ../build/Dockerfile.concourse
-      cp config-lb.yml ../build
+      cp config.yml ../build
   elif [[ "$APPLICATION" == "dp-adot-collector" ]]; then
       cp Dockerfile.concourse ../build
       cp config-aggregator.yml ../build
